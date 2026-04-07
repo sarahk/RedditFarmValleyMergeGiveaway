b84d3e2f4355f20f4b95666279fa58b5338e92d4
Sarah <sarah@itamer.com>
2026-04-07 13:12:49 +1200

    Bump version to 4.08 and update dependency URL

--- RedditFarmValleyMergeGiveaway.user.js
+++ RedditFarmValleyMergeGiveaway.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FarmMergeValley Giveaway Pop-up
-// @version      4.07
+// @version      4.08
 // @updateURL    https://raw.githubusercontent.com/sarahk/RedditFarmValleyMergeGiveaway/main/RedditFarmValleyMergeGiveaway.user.js
 // @downloadURL  https://raw.githubusercontent.com/sarahk/RedditFarmValleyMergeGiveaway/main/RedditFarmValleyMergeGiveaway.user.js
 // @match        *://*.reddit.com/r/FarmMergeValley*
@@ -16,7 +16,7 @@
 // @grant        GM.setValue
 // @grant        GM.deleteValue
 // @run-at       document-start
-// @require      https://fvm.itamer.com/fvm-ui.js?v=1.00
+// @require      https://fvm.itamer.com/fvm-ui.js?v=1.01
 // ==/UserScript==
 
 (function () {
