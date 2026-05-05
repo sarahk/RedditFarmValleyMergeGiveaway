346d368e31178a631934b68f609fc11d31cbb75a
Sarah <sarah@itamer.com>
2026-05-05 16:28:51 +1200

    Bump version to 4.22 and update required script URL

--- RedditFarmValleyMergeGiveaway.user.js
+++ RedditFarmValleyMergeGiveaway.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FarmMergeValley Giveaway Pop-up
-// @version      4.21
+// @version      4.22
 // @updateURL    https://raw.githubusercontent.com/sarahk/RedditFarmValleyMergeGiveaway/main/RedditFarmValleyMergeGiveaway.user.js
 // @downloadURL  https://raw.githubusercontent.com/sarahk/RedditFarmValleyMergeGiveaway/main/RedditFarmValleyMergeGiveaway.user.js
 // @match        *://*.reddit.com/r/FarmMergeValley*
@@ -16,7 +16,7 @@
 // @grant        GM.setValue
 // @grant        GM.deleteValue
 // @run-at       document-start
-// @require      https://fvm.itamer.com/fvm-ui.js?v=1.14
+// @require      https://fvm.itamer.com/fvm-ui.js?v=1.15
 // ==/UserScript==
 
 (function () {
